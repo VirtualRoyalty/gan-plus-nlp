929290a0dd3d3b53e84df249c1880aab61d8eae1
Vadim Alperovich <vadikalperovich@mail.ru>
2022-08-07 05:04:28 +0300

    add simple generator

--- model/generator.py
+++ model/generator.py
@@ -1,5 +1,4 @@
 import torch
-
 from base import *
 
 
@@ -15,7 +14,7 @@ class SimpleGenerator(BaseModel):
         self.dropout_rate = dropout_rate
         layers = self.get_block(input_size, output_size)
         layers.extend(self.get_block(output_size, output_size))
-        self.layers = nn.Sequential(layers)
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, noise: torch.Tensor):
         return self.layers(noise)
@@ -26,4 +25,33 @@ class SimpleGenerator(BaseModel):
         return [nn.Linear(input_size, output_size),
                 nn.LeakyReLU(0.2, inplace=True),
                 nn.Dropout(self.dropout_rate),
-                nn.BatchNorm1d(output_size)]
+                nn.LayerNorm(output_size)]
+
+
+class ContextualGenerator(BaseModel):
+    """Contextual Generator model class with Attention"""
+
+    def __init__(self,
+                 input_size: int,
+                 output_size: int,
+                 dropout_rate: float = 0.2,
+                 **kwargs):
+        super(ContextualGenerator, self).__init__()
+        self.dropout_rate = dropout_rate
+        self.attention = nn.MultiheadAttention(embed_dim=input_size,
+                                               num_heads=1,
+                                               batch_first=True)
+        self.out = nn.Sequential(*self.get_block(input_size, output_size))
+
+    def forward(self, noise: torch.Tensor):
+        context_noise = self.attention(query=noise, key=noise, value=noise,
+                                       need_weights=False)[0]
+        return self.out(context_noise)
+
+    def get_block(self,
+                  input_size: int,
+                  output_size: int):
+        return [nn.Linear(input_size, output_size),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout(self.dropout_rate),
+                nn.LayerNorm(output_size)]
