4b362092a9f1b4d80cdfa40b6daee3495f055dbd
Vadim Alperovich <vadikalperovich@mail.ru>
2022-08-11 04:16:45 +0300

    fix

--- trainer/gan_trainer.py
+++ trainer/gan_trainer.py
@@ -136,7 +136,7 @@ class GANTrainerTokenClassification(BaseTrainer):
         info['total_train_loss'] /= len(self.train_dataloader)
         if verbose:
             print(f"\tTrain loss discriminator: {info['total_train_loss']:.3f}")
-            print(f"\tTrain loss generator: {info['generator_loss']:.3f}")
+            print(f"\tTrain loss generator: {info['total_generator_loss']:.3f}")
 
     def on_epoch_end(self, **kwargs):
         kwargs['train_info']['total_train_loss'] += kwargs['epoch_info']['loss']
