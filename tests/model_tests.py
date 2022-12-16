bc2409d0d8e20f4c1b26b992891459e6497aee3c
Vadim Aperovich <valperovich@ozon.ru>
2022-12-16 23:14:41 +0300

    add first test for models/

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -1,32 +1,32 @@
 import pytest
 import torch
 
+from base import BaseModel
 from model import SimpleGenerator, ContextualGenerator
 from model import DiscriminatorForTokenClassification
 
 from typing import Tuple
 
 
-@pytest.fixture
-def simple_generator_model(input_size: int, output_size: int):
-    return SimpleGenerator(input_size, output_size)
-
-
 @pytest.mark.parametrize(
-    "input_size,seq_len,output_size,expected",
+    "generator, input_size, seq_len, output_size, expected",
     [
-        (100, 1, 200, (1, 200)),
-        (10, 100, 20, (100, 20)),
-        (1, 1, 1, (1, 1)),
+        (SimpleGenerator, 100, 1, 200, (1, 200)),
+        (SimpleGenerator, 10, 100, 20, (100, 20)),
+        (SimpleGenerator, 1, 1, 1, (1, 1)),
+        (ContextualGenerator, 100, 1, 200, (1, 200)),
+        (ContextualGenerator, 10, 100, 20, (100, 20)),
+        (ContextualGenerator, 1, 1, 1, (1, 1)),
     ],
 )
 def test_simple_generator_forward(
+    generator: BaseModel,
     input_size: int,
     seq_len: int,
     output_size: int,
     expected: Tuple[int],
 ):
-    model = SimpleGenerator(input_size, output_size)
+    model = generator(input_size, output_size)
     noise = torch.rand(1, seq_len, input_size)
     ouput = model.forward(noise)
     assert ouput.shape[1:] == expected
