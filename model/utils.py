12cbf78fc9450d07dae238f2999b5dc0f2ecd085
Vadim Alperovich <vadikalperovich@mail.ru>
2023-04-23 17:15:01 +0300

    add some for sequence clf and bug fixes :bug:

--- model/utils.py
+++ model/utils.py
@@ -4,7 +4,13 @@ from dataclasses import dataclass
 from collections import OrderedDict
 from typing import Optional, Tuple, List, Dict
 from datasets import load_metric
-from sklearn.metrics import accuracy, f1_score, precision_score, recall_score, classification_report
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    classification_report,
+)
 
 metric = load_metric("seqeval")
 
@@ -26,7 +32,7 @@ def compute_metrics(predictions: torch.Tensor, labels: List[List[int]], label_na
 
 def compute_clf_metrics(predictions: torch.Tensor, labels: List[int], label_names: List[str]) -> Dict:
     predictions = np.argmax(predictions, axis=1)
-    overall_accuracy = accuracy(labels, predictions)
+    overall_accuracy = accuracy_score(labels, predictions)
     overall_fscore = f1_score(labels, predictions, average="macro")
     overall_precision = precision_score(labels, predictions, average="macro")
     overall_recall = recall_score(labels, predictions, average="macro")
