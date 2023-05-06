f14f62bf407d19415285ff2081ef4f0c83fd646c
Vadim Alperovich <vadikalperovich@mail.ru>
2023-05-07 02:09:54 +0300

    add noise types

--- trainer/gan_trainer.py
+++ trainer/gan_trainer.py
@@ -7,7 +7,7 @@ from torch.optim import Adam, AdamW
 from transformers import AutoModel, get_constant_schedule_with_warmup
 from transformers.trainer_pt_utils import numpy_pad_and_concatenate
 
-from typing import List, Dict, Mapping, Optional
+from typing import List, Dict, Mapping, Optional, Tuple, Literal, Any
 
 from base import BaseTrainer
 from model import (
@@ -56,7 +56,12 @@ class GANTrainerSequenceClassification(BaseTrainer):
             labels=batch["labels"],
             labeled_mask=batch["labeled_mask"],
         )
-        noise = torch.rand(batch_size, self.config["noise_size"], device=self.device)
+        noise = get_noise(
+            (batch_size, self.config["noise_size"]),
+            dist=self.config.get("noise_type", "uniform"),
+            range=self.config.get("noise_range", (-2, 2)),
+            device=self.device,
+        )
         gen_states = self.generator(noise)
 
         fake_output = self.model(external_states=gen_states, input_mask=batch["attention_mask"])
@@ -195,6 +200,59 @@ class GANTrainerSequenceClassification(BaseTrainer):
             )
 
 
+class GANTrainerPairedClassification(GANTrainerSequenceClassification):
+    def training_step(self, batch: Mapping[str, torch.Tensor], log_env: Optional[Dict] = None):
+        batch = self._prepare_inputs(batch)
+        batch_size = batch["input_ids"].shape[0]
+        seq_len = batch["input_ids"].shape[1]
+
+        output = self.model(
+            input_ids=batch["input_ids"],
+            input_mask=batch["attention_mask"],
+            labels=batch["labels"],
+            labeled_mask=batch["labeled_mask"],
+        )
+        noise = get_noise(
+            (batch_size, self.config["noise_size"]),
+            dist=self.config.get("noise_type", "uniform"),
+            range=self.config.get("noise_range", (-2, 2)),
+            device=self.device,
+        )
+        gen_states = self.generator(noise)
+
+        fake_output = self.model(external_states=gen_states, input_mask=batch["attention_mask"])
+
+        # Generator loss estimation
+        cheat_loss = -1 * torch.mean(torch.log(1 - fake_output.probs[:, -1] + self.config["epsilon"]))
+        feat_sim_loss = torch.mean(
+            torch.pow(
+                torch.mean(output.hidden_states, dim=0) - torch.mean(fake_output.hidden_states, dim=0), 2
+            )
+        )
+        generator_loss = cheat_loss + feat_sim_loss
+
+        # Discriminator loss estimation
+        unsup_fake_loss = fake_output.loss
+        unsup_real_loss = -torch.mean(torch.log(1 - output.probs[:, -1] + self.config["epsilon"]))
+        discriminator_loss = output.loss + unsup_fake_loss + unsup_real_loss
+
+        self._train_logging(log_env, dloss=discriminator_loss, gloss=generator_loss)
+
+        self.generator_optimizer.zero_grad()
+        self.optimizer.zero_grad()
+        generator_loss.backward(retain_graph=True)
+        discriminator_loss.backward()
+        self.generator_optimizer.step()
+        self.optimizer.step()
+
+        if self.config["apply_scheduler"]:
+            self.scheduler.step()
+            self.generator_scheduler.step()
+
+        epoch_info = dict(loss=discriminator_loss.item(), generator_loss=generator_loss.item())
+        return epoch_info
+
+
 class GANTrainerTokenClassification(BaseTrainer):
     """GAN trainer for token classification tasks"""
 
@@ -232,12 +290,22 @@ class GANTrainerTokenClassification(BaseTrainer):
             labeled_mask=batch["labeled_mask"],
         )
         if self.config["GAN_TYPE"] != "mixed":
-            noise = torch.rand(batch_size, seq_len, self.config["noise_size"], device=self.device)
+            noise = get_noise(
+                (batch_size, seq_len, self.config["noise_size"]),
+                dist=self.config.get("noise_type", "uniform"),
+                range=self.config.get("noise_range", (-2, 2)),
+                device=self.device,
+            )
             gen_states = self.generator(noise)
         else:
             mixed_fake_ratio = self.config.get("mixed_fake_ratio", 0.3)
             noise_len = int(seq_len * mixed_fake_ratio)
-            noise = torch.rand(batch_size, noise_len, self.config["noise_size"], device=self.device)
+            noise = get_noise(
+                (batch_size, noise_len, self.config["noise_size"]),
+                dist=self.config.get("noise_type", "uniform"),
+                range=self.config.get("noise_range", (-2, 2)),
+                device=self.device,
+            )
             hstates_len = seq_len - noise_len
             rand_indexes = torch.randperm(seq_len)[:hstates_len]
             selected_hiden_states = output.hidden_states[:, rand_indexes, :]
@@ -375,3 +443,17 @@ class GANTrainerTokenClassification(BaseTrainer):
             self.generator_scheduler = _default_scheduler(
                 self.generator_optimizer, num_warmup_steps=self.config["num_warmup_steps_g"]
             )
+
+
+def get_noise(
+    sizes: Tuple[int],
+    dist: Literal["uniform", "normal"] = "uniform",
+    range: Tuple[float] = (-2, 2),
+    device: Any = None,
+) -> torch.FloatTensor:
+    assert dist in ["uniform", "normal"]
+    noise = torch.zeros(*sizes, device=device)
+    if dist == "uniform":
+        return noise.uniform_(*range)
+    elif dist == "normal":
+        return noise.normal_(*range)
